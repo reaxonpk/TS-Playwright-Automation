890072b2aca35e05d9467104cbfc5666df3e078e
reaxonpk <reaxonpk1234@gmail.com>
2025-04-15 11:11:45 +0300

    Page-Object-Model-Playwright

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 import * as dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+dotenv.config();
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -26,12 +26,9 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-    //run the tests in headed mode depends on the boolean
-    headless: false,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    baseURL: process.env.SAUCE_BASE_URL, // Enables relative URLs in tests
+    headless: false,                     // Runs tests with browser UI
+    trace: "on-first-retry",             // Captures trace only if test fails the first time
   },
 
   /* Configure projects for major browsers */
---
+++ playwright/test-data/messages.ts
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGES = {
+  locked: "Sorry, this user has been locked out.",
+  invalid: "Username and password do not match",
+};
